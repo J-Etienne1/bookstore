0be8dd19b618c92084ec6762468b2a99f60fb417
Jason <jasonetienne1@gmail.com>
2024-02-29 19:41:33 +0000

    cleaned up GET response

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,11 @@ import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+
+
+
+
+
 // Middlewear for parsing request body
 app.use(express.json());
 
@@ -12,6 +17,12 @@ app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("It works");
 });
+
+
+
+
+
+
 
 // Route for Saving a new Book
 app.post("/books", async (request, response) => {
@@ -30,10 +41,28 @@ app.post("/books", async (request, response) => {
       author: request.body.author,
       publishYear: request.body.publishYear,
     };
-    const book = await Book.create(newBook)
+    const book = await Book.create(newBook);
     return response.status(201).send(book);
   } catch (error) {
     return response.status(500).send({ message: error.message });
+  }
+});
+
+
+
+
+
+// Route for getting all books from MongoDB
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).send({
+        count: books.length,
+        data: books,
+    });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
   }
 });
 
