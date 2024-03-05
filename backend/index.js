2ed108070134e9fd3b927e8717368c19884b6c47
Jason <jasonetienne1@gmail.com>
2024-03-05 19:27:56 +0000

    completed working GET individual book by ID

--- backend/index.js
+++ backend/index.js
@@ -67,6 +67,25 @@ app.get("/books", async (request, response) => {
   }
 });
 
+
+
+
+// Route to get an individual book by ID from MongoDB
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+    
+    return response.status(200).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+
+
 mongoose
   .connect(mongoDBURL)
   .then(() => {
