53124be80a8101d1bb681d443835510954ff2f3f
Jason <jasonetienne1@gmail.com>
2024-03-10 14:02:02 +0000

    finished setting up pages, paths and elements for routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { CreateBooks } from "./pages/CreateBooks";
+import { DeleteBooks } from "./pages/DeleteBooks";
+import { EditBooks } from "./pages/EditBooks";
+import { Home } from "./pages/Home";
+import { ShowBooks } from "./pages/ShowBooks";
 
 export const App = () => {
   <Routes>
-    <Route path="" element={} />
-    <Route path="" element={} />
-    <Route path="" element={} />
-    <Route path="" element={} />
-    <Route path="" element={} />
+    <Route path="/" element={ <Home /> } />
+    <Route path="/books/create" element={ <CreateBooks /> } />
+    <Route path="/books/details/:id" element={ <ShowBooks /> } />
+    <Route path="/books/edit/:id" element={ <EditBooks /> } />
+    <Route path="/books/delete/:id" element={ <DeleteBooks /> } />
   </Routes>
 };
 
